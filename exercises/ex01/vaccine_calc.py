77630d698f5acdb65803eb68660d7b0b61a10874
Will Astilla <wastilla@email.unc.edu>
2021-02-04 18:58:51 -0500

    progress on excersise 1

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730387741"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,32 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+population = input("Population: ")
+doses_administered = input("Doses administered: ")
+doses_per_day = input("Doses per day: ")
+target_percent = input("Target percent vaccinated: ")
+
+population = int(population)
+doses_administered = int(doses_administered)
+doses_per_day = int(doses_per_day)
+target_percent = int(target_percent)/100
+
+target_population = population * target_percent
+pop_without_vaccine = population - (doses_administered/2)
+days_until_target = ((pop_without_vaccine - target_population)*2)/doses_per_day
+
+today: datetime = datetime.today()
+number_of_days: timedelta = timedelta(days_until_target)
+target_date: datetime = today + number_of_days
+target_date = target_date.strftime("%B %d, %Y")
+
+target_percent = target_percent * 100
+target_percent = round(target_percent)
+days_until_target = round(days_until_target)
+target_percent = str(target_percent)
+days_until_target = str(days_until_target)
+target_date = str(target_date)
+
+print("We will reach " + target_percent + "% vaccination in " + days_until_target + " days, which falls on " + target_date  + ".")
+
+
