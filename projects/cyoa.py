af4b73ecd2d7b6c28b6bac9e534e4a89ea9ff523
Will Astilla <wastilla@email.unc.edu>
2021-02-25 18:41:58 -0500

    Project 00 progress

---
+++ projects/cyoa.py
@@ -0,0 +1,60 @@
+
+from random import randint
+player: str = ""
+points: int 
+
+def main() -> None:
+    global points
+    points = 0
+    greet()
+    stillPlaying: bool = True
+    while stillPlaying == True:
+        print(f"Hi, {player}. What do you want to do?")
+        print("1. 3-sided die guessing game 2. 6-sided die guessing 3. Stop Playing")
+        userChoice: int = int(input("Enter the number corresponding with what you want to do: "))
+        if userChoice == 1:
+            threeSidedDiePoints()
+        if userChoice == 2:
+            points += sixSidedDiePoints(points)
+        if userChoice == 3:
+            print(f"Thanks for playing {player}.")
+            print(f"You scored {points}.")
+            stillPlaying = False
+
+def threeSidedDiePoints() -> None:
+    global points
+    roll: int = randint(1, 3)
+    print(f"Hi, {player}. Which number do you think the die landed on?")
+    guess: int = int(input("Enter a number 1-3: "))
+    if guess == roll:
+        points += 1
+        print("Correct!")
+    else:
+        print("You guessed wrong!")
+        points = 0
+
+def sixSidedDiePoints(points: int) -> int:
+    roll: int = randint(1, 6)
+    print(f"Hi, {player}. Which number do you think the die landed on?")
+    guess: int = int(input("Enter a number 1-6: "))
+    if guess == roll:
+        points += 2
+        print("Correct!")
+    else:
+        print("You guessed wrong!")
+        points = 0
+    return points
+    
+    
+def greet() -> None:
+    global player
+    player = input("Enter Your Name: ")
+    print(f"Welcome {player}.  The objective of this game is to guess the outcome of a three sided die roll.")
+    print("You gain a point for each dice roll you guess correctly")
+    print("If you're feeling lucky, you can choose to roll a six sided die instead.")
+    print("Each roll you guess correctly from a six sided die awards you double the points!")
+    print("If you guess incorrectly your points reset to zero")
+    print("Lets see how many points you can get!")
+
+if __name__ == "__main__":
+    main()
