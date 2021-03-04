5d6b3a04eb2eacceddf398047ce66cc09ac0b352
Will Astilla <wastilla@email.unc.edu>
2021-03-03 20:51:18 -0500

    EX03 Progress

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,13 +1,28 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730387741"
 
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(avoid_fifth("hello there"))
+    print(avoid_fifth("steezy"))
 
+def avoid_fifth(y: str) -> str:
+    i: int = 0
+    newString: str = ""
+    temp: chr
+    while i < len(y):
+        temp = y[i]  
+        if temp != "e" and temp != "E":
+            newString += temp 
+            i+=1
+        else:
+            i+=1
+    return str(newString)
+
+        
 
 if __name__ == "__main__":
     main()
