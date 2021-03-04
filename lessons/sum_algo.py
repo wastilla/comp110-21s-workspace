8a1728eeb632be0e11ac3b10abca874c1ffe4b65
Will Astilla <wastilla@email.unc.edu>
2021-03-03 20:03:16 -0500

    sum algo lesson and project 00 progress

---
+++ lessons/sum_algo.py
@@ -0,0 +1,19 @@
+"""Examples of list and loop algorithm."""
+
+def sum_algo(xs: list[int]) -> int:
+    """Summation of input list is returned."""
+    # 1. Initialize total and index i to 0
+    total: int = 0
+    i: int = 0
+    # 2. While index i is valid, meaning i < len(xs)
+    while i < len(xs):
+        # 2. True) take xs[i] and add to total
+        total += xs[i]
+        # 2. True) increase i by 1, moving it to next index variable
+        i += 1
+    #   2. False) Result is stored in total varible
+    return total
+
+odds: list[int] = [1, 3, 5, 7]
+odds_sum: int = sum_algo(odds)
+print(odds_sum)
--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,21 +1,22 @@
-"""This program is dice guessing game. Users choose to roll either a 3 or 6-sided die.
-They are rewarded 1 point for correctly guessing the outcome of a 3-sided die roll, and
-2 points for correctly guessing the outcome of a 6-sided die roll. Players' points reset to
-zero if they guess inforrectly. A game loop is incorporated in the main function.  The loop 
-allows users to continue playing either game if they wish, or quit playing altogether.
-Players are awarded a trophy if they win 3 or more points"""
+"""This program is an interactive dice roll guessing game."""
+
 from random import randint
+
+__author__ = "730387741"
+
 player: str = ""
 points: int 
 winners_trophy: str = "\U0001F3C6"
 sad_face: str = "\U0001F641"
 
+
 def main() -> None:
+    """The entrypoint of the program."""
     global points
     points = 0
     greet()
     stillPlaying: bool = True
-    while stillPlaying == True:
+    while stillPlaying:
         print(f"So, {player}. What do you want to do?")
         print("1. 3-sided die guessing game 2. 6-sided die guessing 3. Stop Playing")
         userChoice: int = int(input("Enter the number corresponding with what you want to do: "))
@@ -26,23 +27,22 @@ def main() -> None:
         if userChoice == 3:
             if points >= 3:
                 print(f"Thanks for playing {player}.")
-                print(f"You scored {points}!! That's amazing!")
+                print(f"You scored {points} points!! That's amazing!")
                 print(f"You earned a trophy {winners_trophy}!")
                 stillPlaying = False
             if points == 0:
                 print(f"Thanks for playing {player}.")
-                print(f"You scored {points}.")
+                print(f"You scored {points} points.")
                 print(f"Better luck next time {sad_face}")
                 stillPlaying = False
             else:
                 print(f"Thanks for playing {player}.")
-                print(f"You scored {points}.")
+                print(f"You scored {points} point(s).")
                 stillPlaying = False
 
+
 def threeSidedDiePoints() -> None:
-    """This function allows users to guess the outcome of a 3-sided die roll
-    players are awarded 1 point for guessing correctly. If they guess 
-    incorrectly, their points are reset to 0."""
+    """This function asks users to guess the outcome of a 3-sided die roll."""
     global points
     roll: int = randint(1, 3)
     print(f"Hi, {player}. Which number do you think the die landed on?")
@@ -54,10 +54,9 @@ def threeSidedDiePoints() -> None:
         print("You guessed wrong!")
         points = 0
 
+
 def sixSidedDiePoints(points: int) -> int:
-    """This function allows users to guess the outcome of a 6-sided die roll
-    players are awarded 2 points for guessing correctly. If they guess 
-    incorrectly, their points are reset to 0."""
+    """This function asks users to guess the outcome of a 6-sided die roll."""
     roll: int = randint(1, 6)
     print(f"Hi, {player}. Which number do you think the die landed on?")
     guess: int = int(input("Enter a number 1-6: "))
@@ -71,7 +70,7 @@ def sixSidedDiePoints(points: int) -> int:
     
     
 def greet() -> None:
-    """This function asks a players name, greets them, and gives the instructions for the game"""
+    """This function asks the player's name, greets them, and gives the instructions for the game."""
     global player
     player = input("Enter Your Name: ")
     print(f"Welcome {player}.  The objective of this game is to guess the outcome of a three sided die roll.")
@@ -81,6 +80,7 @@ def greet() -> None:
     print("If you guess incorrectly your points reset to zero")
     print("If you get 3 points or more, you win a trophy!")
     print("Lets see how many points you can get!")
+
 
 if __name__ == "__main__":
     main()
